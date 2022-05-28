d4b015286fab5d24ace10f23855dc2f82de07233
Volodymyr Hovorun <volodymyr.hovorun@agritask.com>
2022-05-28 20:20:57 +0300

    Added unit test and checklist

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,38 @@
-import { createUserPayload, formatDate } from '../src/main'
-import { expect, test } from '@jest/globals'
-
+import {createUserPayload, formatDate} from '../src/main'
+import {expect, test} from '@jest/globals'
 
 test('Valid user created', async () => {
-    const currentDate = new Date();
-    const expirationDate = currentDate;
-    expirationDate.setMonth(currentDate.getMonth() + 12);
-    expect(createUserPayload('Volodymyr', 'Hovorun')).toStrictEqual({
-        firstName: 'Volodymyr',
-        lastName: 'Hovorun',
-        createdAt: formatDate(currentDate),
-        validUntil: formatDate(expirationDate)
-    });
+  const currentDate = new Date()
+  const expirationDate = currentDate
+  expirationDate.setMonth(currentDate.getMonth() + 12)
+  expect(createUserPayload('Volodymyr', 'Hovorun')).toStrictEqual({
+    firstName: 'Volodymyr',
+    lastName: 'Hovorun',
+    createdAt: formatDate(currentDate),
+    validUntil: formatDate(expirationDate)
+  })
 })
+
+test('Data is not received from FE', async () => {
+  const currentDate = new Date()
+  const expirationDate = currentDate
+  expirationDate.setMonth(currentDate.getMonth() + 12)
+  expect(createUserPayload('', '')).toThrowError('No params were passed');
+})
+
 
 test('Format method check', async () => {
-    const currentDate = new Date();
-    expect(formatDate(currentDate)).toBe(currentDate.toISOString().slice(0, 10));
+  const currentDate = new Date()
+  expect(formatDate(currentDate)).toBe(currentDate.toISOString().slice(0, 10))
 })
 
-
 test('Format method check if null', async () => {
-    expect(formatDate(null)).toBe('');
+  expect(formatDate(null)).toBe('')
 })
 
 test('Format method check if undefined', async () => {
-    expect(formatDate(undefined)).toBe('');
+  expect(formatDate(undefined)).toBe('')
 })
+
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 export function createUserPayload(firstName: string, lastName: string): object {
-  const currentDate = new Date();
-  const expirationDate = currentDate;
-  expirationDate.setMonth(currentDate.getMonth() + 12);
-  const user = { firstName, lastName, createdAt: formatDate(currentDate), validUntil: formatDate(expirationDate)};
+  const currentDate = new Date()
+  const expirationDate = currentDate
+  expirationDate.setMonth(currentDate.getMonth() + 12)
+  const user = {
+    firstName,
+    lastName,
+    createdAt: formatDate(currentDate),
+    validUntil: formatDate(expirationDate)
+  }
   // TODO implement BE POST request
-  return user;
+  return user
 }
 
-export const formatDate = (date?: Date| null ): string => {
-  if(date === null || date === undefined) return '';
+export const formatDate = (date?: Date | null): string => {
+  if (date === null || date === undefined) return ''
 
-  return date.toISOString().slice(0, 10);
-};
-
+  return date.toISOString().slice(0, 10)
+}
